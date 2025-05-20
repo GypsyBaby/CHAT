d84f2643929b21340a51a6531548acc6af371662
Sergei Dmitriev <s.dmitriev@adriver.ru>
2025-05-20 23:25:36 +0300

    added base exception middleware

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import asyncio
+import logging
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 
 from src.api.auth import auth_router
 from src.api.chat import chat_router, websocket_router
@@ -9,7 +10,18 @@ from src.api.user import user_router
 
 from src import sync_persistent_and_memory_chat_storage
 
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
+
+@app.middleware
+async def base_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except BaseException as exc:
+        logger.exception("An unexpected error occurred")
+        raise HTTPException(status_code=500, detail="Call Sergey Dmitriev, smth went wrong")
+
 
 app.include_router(user_router)
 app.include_router(chat_router)
